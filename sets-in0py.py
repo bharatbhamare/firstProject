a31d438ef6bdce712e1d62af72548ece44d5aefb
bharat.bhamare <b3.aug95@gmail.com>
2018-10-24 09:25:14 +0530

    sets example

---
+++ sets-in0py.py
@@ -0,0 +1,8 @@
+someSet = {"milk", "beer", "drink", "foo","bar","buz"}
+
+print (someSet)
+
+if "someItem" in someSet:
+    print("you already Have Some Item")
+else:
+    print("u need to bye some item")
